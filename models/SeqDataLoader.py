b27aeb01510f72c5166ea5d7bafe003d68d22796
snakeztc <zhaotiancheng.hz@gmail.com>
2016-09-30 11:44:14 -0400

    update seq data feed

--- models/SeqDataLoader.py
+++ models/SeqDataLoader.py
@@ -2,23 +2,20 @@ import numpy as np
 
 
 class SeqDataLoader(object):
+    batch_size = 0
+    ptr = 0
+    num_batch = None
+    batch_indexes = None
+
     def __init__(self, name, data):
         self.name = name
         self.data = data
-        self.indexes = range(len(self.data))
         self.data_size = len(data)
-
-        # iteration related
-        self.ptr = 0
-        self.batch_size = 0
-        self.num_batch = None
-
-        # find the longest sequence length
-        max_seq_len = np.max([len(row) for row in data])
-        print "Longest sentence in corpus is %d" % max_seq_len
+        all_lens = [len(line) for line in self.data]
+        self.sorted_indexes = list(np.argsort(all_lens))
 
     def _shuffle(self):
-        np.random.shuffle(self.indexes)
+        np.random.shuffle(self.batch_indexes)
 
     def _prepare_batch(self, selected_index):
         rows = [self.data[idx] for idx in selected_index]
@@ -32,16 +29,21 @@ class SeqDataLoader(object):
         return inputs, input_lens, outputs
 
     def epoch_init(self, batch_size, shuffle=True):
-        if shuffle:
-            self._shuffle()
         self.ptr = 0
         self.batch_size = batch_size
         self.num_batch = self.data_size // batch_size
+
+        self.batch_indexes = []
+        for i in range(self.num_batch):
+            self.batch_indexes.append(self.sorted_indexes[i*self.batch_size:(i+1)*self.batch_size])
+
+        if shuffle:
+            self._shuffle()
         print("%s begins training with %d batches" % (self.name, self.num_batch))
 
     def next_batch(self):
         if self.ptr < self.num_batch:
-            selected_ids = self.indexes[self.ptr * self.batch_size:min((self.ptr+1)*self.batch_size, self.data_size)]
+            selected_ids = self.batch_indexes[self.ptr]
             self.ptr += 1
             return self._prepare_batch(selected_index=selected_ids)
         else:
