67d02796566d7ecd8e40d4d51e6cd651548b383f
snakeztc <zhaotiancheng.hz@gmail.com>
2016-09-30 00:08:50 -0400

    chagne batch giving to make sure each batch has same length

--- models/StateDataLoader.py
+++ models/StateDataLoader.py
@@ -6,6 +6,7 @@ class StateDataLoader(object):
     ptr = 0
     batch_size = 0
     num_batch = None
+    batch_indexes = None
 
     def __init__(self, name, data):
         self.name = name
@@ -14,13 +15,13 @@ class StateDataLoader(object):
         for line in data:
             for t_id in range(2, len(line)):
                 self.data.append(line[0:t_id])
-
-        self.indexes = range(len(self.data))
+        all_lens = [len(line) for line in self.data]
+        self.sorted_indexes = list(np.argsort(all_lens))
         self.data_size = len(self.data)
         print("Create %d sentence state samples" % self.data_size)
 
     def _shuffle(self):
-        np.random.shuffle(self.indexes)
+        np.random.shuffle(self.batch_indexes)
 
     def _prepare_batch(self, selected_index):
         rows = [self.data[idx] for idx in selected_index]
@@ -34,16 +35,18 @@ class StateDataLoader(object):
         return inputs, input_lens, outputs
 
     def epoch_init(self, batch_size, shuffle=True):
-        if shuffle:
-            self._shuffle()
+        # create batch indexes for computation efficiency
         self.ptr = 0
         self.batch_size = batch_size
         self.num_batch = self.data_size // batch_size
+        self.batch_indexes = []
+        for i in range(self.num_batch):
+            self.batch_indexes.append(self.sorted_indexes[i*self.batch_size:(i+1)*self.batch_size])
         print("%s begins training with %d batches" % (self.name, self.num_batch))
 
     def next_batch(self):
         if self.ptr < self.num_batch:
-            selected_ids = self.indexes[self.ptr * self.batch_size:min((self.ptr+1)*self.batch_size, self.data_size)]
+            selected_ids = self.batch_indexes[self.ptr]
             self.ptr += 1
             return self._prepare_batch(selected_index=selected_ids)
         else:
